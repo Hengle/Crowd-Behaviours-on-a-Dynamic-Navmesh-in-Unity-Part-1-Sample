951bb1af80fa5bf390882faefd8c00f36b6fbc9b
mage.wang <wangm4a1@hotmail.com>
2018-05-04 01:05:55 +0800

    sample

---
+++ Assets/Agent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour {
+    public Transform target;
+    private NavMeshAgent navAgent;
+	// Use this for initialization
+	void Start () {
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
